bee6a89b42b590881393ddf01c89cd5106b27aee
Miguel Gonzalez <migolo10@gmail.com>
2016-08-19 16:49:52 -0500

    Search a Label instead of using a container name

--- nginxhup.go
+++ nginxhup.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/docker/engine-api/client"
+	"github.com/docker/engine-api/types"
+	"github.com/docker/engine-api/types/filters"
 	"golang.org/x/net/context"
 )
 
@@ -13,29 +15,36 @@ func main() {
 	argsWithoutProg := os.Args[1:]
 
 	if len(argsWithoutProg) < 1 {
-		fmt.Fprintln(os.Stderr, "No container specified")
+		fmt.Fprintln(os.Stderr, "No Label String specified Example: com.intzen.nginx_name=MainNginx")
 		os.Exit(1)
 	}
 
-	containerName := argsWithoutProg[0]
+	containerLabelFilter := argsWithoutProg[0]
 	defaultHeaders := map[string]string{"User-Agent": "engine-api-cli-1.0"}
 	cli, err := client.NewClient("unix:///var/run/docker.sock", "v1.22", nil, defaultHeaders)
 	if err != nil {
 		panic(err)
 	}
 
-	errorKill := cli.ContainerKill(context.Background(), containerName, "HUP")
-	if errorKill != nil {
-		panic(errorKill)
+	filters := filters.NewArgs()
+	filters.Add("label", containerLabelFilter)
+
+	containers, errorGetContainers := cli.ContainerList(context.Background(), types.ContainerListOptions{
+		Filter: filters,
+	})
+
+	if errorGetContainers != nil {
+		panic(errorGetContainers)
 	}
-
-	fmt.Println("HUP sent")
-	/*
-		information, errInspect := cli.ContainerInspect(context.Background(), containerName)
-		if errInspect != nil {
-			panic(errInspect)
+	if len(containers) > 0 {
+		for _, container := range containers {
+			errorKill := cli.ContainerKill(context.Background(), container.ID, "HUP")
+			if errorKill != nil {
+				panic(errorKill)
+			}
+			fmt.Println("HUP sent to " + container.ID)
 		}
-
-		fmt.Println(information.ID)
-	*/
+	} else {
+		fmt.Println("No containers found for label search " + containerLabelFilter)
+	}
 }
